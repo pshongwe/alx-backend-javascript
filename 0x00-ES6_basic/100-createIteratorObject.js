9b610829ef05d41e129f81047e43b79b80e61e3f
Pibo Shongwe <piboman@yahoo.com>
2024-05-28 04:17:06 +0000

    u

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,9 @@
+export default function createIteratorObject(report) {
+  const allEmployees = [];
+
+  for (const department of Object.values(report.allEmployees)) {
+    allEmployees.push(...department);
+  }
+
+  return allEmployees[Symbol.iterator]();
+}
