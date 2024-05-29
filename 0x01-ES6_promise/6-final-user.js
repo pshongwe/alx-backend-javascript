2611e2575ed638473238d1f94f5b7084bb902a24
Pibo Shongwe <piboman@yahoo.com>
2024-05-29 22:04:14 +0000

    update

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,9 +5,9 @@ export default async function handleProfileSignup(firstName, lastName, fileName)
   return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((response) => (
-       response.map((o) => ({
-         status: o.status,
-         value: o.status === 'fulfilled' ? o.value : String(o.reason),
-       }))
+      response.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }))
     ));
 }
