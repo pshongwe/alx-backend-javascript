81862fc05603ef929980fc3705bbcf5a572b8fe7
Pibo Shongwe <piboman@yahoo.com>
2024-05-30 19:31:04 +0000

    update

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -10,9 +10,9 @@ export default class HolbertonCourse {
    * @param {String[]} students - Students (names) taking the course.
    */
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
   /**
