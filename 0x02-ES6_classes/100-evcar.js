972cbf6d792ca3030f845694f4a9572b8a8ec852
Pibo Shongwe <piboman@yahoo.com>
2024-05-30 23:00:31 +0000

    update

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,43 @@
+import Car from './10-car.js';
+
+/**
+ * Class representing an electric vehicle car.
+ * @extends Car
+ */
+export default class EVCar extends Car {
+  /**
+   * Create an electric vehicle car.
+   * @param {string} brand - The brand of the car.
+   * @param {string} motor - The motor type of the car.
+   * @param {string} color - The color of the car.
+   * @param {string} range - The range of the car.
+   */
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  /**
+   * Get the range of the car.
+   * @return {string} The range of the car.
+   */
+  get range() {
+    return this._range;
+  }
+
+  /**
+   * Set the range of the car.
+   * @param {string} value - The range of the car.
+   */
+  set range(value) {
+    this._range = value;
+  }
+
+  /**
+   * Clone the car.
+   * @return {Car} A new Car object.
+   */
+  cloneCar() {
+    return new Car(this._brand, this._motor, this._color);
+  }
+}
