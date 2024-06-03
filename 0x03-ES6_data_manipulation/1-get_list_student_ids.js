c6d0327037691f86aef1192136e39de4229c4c8f
Pibo Shongwe <piboman@yahoo.com>
2024-06-04 00:17:44 +0200

    Update 1-get_list_student_ids.js

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,6 @@
 export default function getListStudentIds(students) {
   if (Array.isArray(students)) {
-    return students.map((student) => student.id);
+    return students.map((s) => s.id);
   }
   return [];
 }
