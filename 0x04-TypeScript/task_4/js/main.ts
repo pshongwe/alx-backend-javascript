bc4f1bbbdec0b06e8aa5b3a8e7558bb768dab0d3
Pibo Shongwe <piboman@yahoo.com>
2024-06-06 03:06:32 +0000

    update

---
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -0,0 +1,27 @@
+/// <reference path="./Cpp.ts" />
+/// <reference path="./Java.ts" />
+/// <reference path="./React.ts" />
+
+export const cpp: Subjects.Cpp = new Subjects.Cpp();
+export const java: Subjects.Java = new Subjects.Java();
+export const react: Subjects.React = new Subjects.React();
+export const cTeacher: Subjects.Teacher = {
+  firstName: 'Bjarne',
+  lastName: 'Stroustrup',
+  experienceTeachingC: 5,
+};
+
+console.log('C++');
+cpp.setTeacher = cTeacher;
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
+
+console.log('Java');
+java.setTeacher = cTeacher;
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
+
+console.log('React');
+react.setTeacher = cTeacher;
+console.log(react.getRequirements());
+console.log(react.getAvailableTeacher());
