01f82da1bae6a957c9bf12492100f25e9bd9e1d9
Pibo Shongwe <piboman@yahoo.com>
2024-07-23 16:19:19 +0000

    u

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,9 @@
 console.log('Welcome to Holberton School, what is your name?');
-process.stdin.setEncoding('utf8');
 process.stdin.on('data', (input) => {
   const name = input.trim();
   console.log(`Your name is: ${name}`);
   process.stdin.end();
 });
 process.on('exit', () => {
-    console.log('This important software is now closing');
+  console.log('This important software is now closing');
 });
