f883250f02b700a8ef8d7efd9a9f363641da9860
Pibo Shongwe <piboman@yahoo.com>
2024-07-24 00:32:45 +0000

    u

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,4 +1,4 @@
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?');
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
   if (name) {
