9662017dbe0bf1f3b17568bab662778e6c9777d7
Pibo Shongwe <piboman@yahoo.com>
2024-07-24 00:47:25 +0000

    u

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,7 +1,12 @@
 const fs = require('fs');
 
 function countStudents(path) {
-  try {
+    if (!fs.existsSync(path)) {
+      throw new Error('Cannot load the database');
+    }
+    if (!fs.statSync(path).isFile()) {
+      throw new Error('Cannot load the database');
+    }    
     const data = fs.readFileSync(path, 'utf8');
     const lines = data.split('\n').filter(line => line.trim() !== '');
     const students = lines.slice(1);
@@ -21,9 +26,6 @@ function countStudents(path) {
     for (const [field, names] of Object.entries(fields)) {
       console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
     }
-  } catch (error) {
-    throw new Error('Cannot load the database');
-  }
 }
 
 module.exports = countStudents;
