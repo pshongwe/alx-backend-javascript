970891ee0bbad4801e8d4b42728da0901b761fea
Pibo Shongwe <piboman@yahoo.com>
2024-07-25 17:29:40 +0000

    update

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,28 @@
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
+
+describe('calculateNumber', () => {
+  it('should return 4 when inputs are 1 and 3', () => {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+  });
+
+  it('should return 5 when inputs are 1 and 3.7', () => {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
+
+  it('should return 5 when inputs are 1.2 and 3.7', () => {
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  });
+
+  it('should return 6 when inputs are 1.5 and 3.7', () => {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+
+  it('should return 0 when inputs are -1.4 and 1.4', () => {
+    assert.strictEqual(calculateNumber(-1.4, 1.4), 0);
+  });
+
+  it('should return -1 when inputs are -1.5 and -0.5', () => {
+    assert.strictEqual(calculateNumber(-1.5, -0.5), -1);
+  });
+});
