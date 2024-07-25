d73094070c5848b02bc0673b5e07edbd33d2614c
Pibo Shongwe <piboman@yahoo.com>
2024-07-25 20:03:20 +0000

    u

--- 0x06-unittests_in_js/4-payment.js
+++ 0x06-unittests_in_js/4-payment.js
@@ -3,6 +3,6 @@ const Utils = require('./utils');
 const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
   const total = Utils.calculateNumber('SUM', totalAmount, totalShipping);
   console.log(`The total is: ${total}`);
-}
+};
 
 module.exports = sendPaymentRequestToApi;
