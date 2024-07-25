5b0b16f281a644b0032516e1069630897a1f7720
Pibo Shongwe <piboman@yahoo.com>
2024-07-25 19:23:59 +0000

    u

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -4,24 +4,15 @@ const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./4-payment');
 
 describe('sendPaymentRequestToApi', () => {
-  let stub;
-  let consoleSpy;
-
-  beforeEach(function () {
-    stub = sinon.stub(Utils, 'calculateNumber').returns(10);
-    consoleSpy = sinon.spy(console, 'log');
-  });
-
-  afterEach(function () {
-    stub.restore();
-    consoleSpy.restore();
-  });
-
   it('should call Utils.calculateNumber with "SUM", 100, and 20', () => {
+    const stub = sinon.stub(Utils, 'calculateNumber').returns(10);
+    const consoleSpy = sinon.spy(console, 'log');
     sendPaymentRequestToApi(100, 20);
     expect(stub.calledWith('SUM', 100, 20)).to.be.true;
     expect(stub.callCount).to.be.equal(1);
     expect(consoleSpy.calledOnceWith('The total is: 10')).to.be.true;
     expect(consoleSpy.callCount).to.be.equal(1);
+    stub.restore();
+    consoleSpy.restore();
   });
 });
