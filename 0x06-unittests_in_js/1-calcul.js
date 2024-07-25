bb8f6cc89c486ce0501a219f80dc40735575430d
Pibo Shongwe <piboman@yahoo.com>
2024-07-25 17:33:39 +0000

    u

---
+++ 0x06-unittests_in_js/1-calcul.js
@@ -0,0 +1,26 @@
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+
+describe('calculateNumber', () => {
+  describe('SUM', () => {
+    it('should return 6 when inputs are 1.4 and 4.5', () => {
+      assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+    });
+  });
+
+  describe('SUBTRACT', () => {
+    it('should return -4 when inputs are 1.4 and 4.5', () => {
+      assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+    });
+  });
+
+  describe('DIVIDE', () => {
+    it('should return 0.2 when inputs are 1.4 and 4.5', () => {
+      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+    });
+
+    it('should return "Error" when inputs are 1.4 and 0', () => {
+      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+    });
+  });
+});
