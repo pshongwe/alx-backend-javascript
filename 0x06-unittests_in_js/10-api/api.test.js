c70e349dd626757222a3ea3e55b1d12c40d0ee92
Pibo Shongwe <piboman@yahoo.com>
2024-07-25 21:26:25 +0000

    u

---
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -0,0 +1,81 @@
+const request = require('request');
+const { expect } = require('chai');
+const BASE_URL = 'http://localhost:7865';
+
+describe('Index page', function () {
+  it('GET / returns correct response', (done) => {
+    request.get(`${BASE_URL}/`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
+  it('should return status code 200 for valid ID', function (done) {
+    request.get(`${BASE_URL}/cart/12`, (error, response) => {
+      if (error) return done(error);
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('should return the correct message for valid ID', function (done) {
+    request.get(`${BASE_URL}/cart/12`, (error, response, body) => {
+      if (error) return done(error);
+        expect(body).to.equal('Payment methods for cart 12');
+        done();
+      });
+  });
+
+  it('should return status code 404 for invalid ID', function (done) {
+    request.get(`${BASE_URL}/cart/hello`, (error, response) => {
+      if (error) return done(error);
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+
+  it('should return status code 200 for /available_payments', function (done) {
+    request.get(`${BASE_URL}/available_payments`, (error, response) => {
+      if (error) return done(error);
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('should return correct JSON for /available_payments', function (done) {
+    request.get(`${BASE_URL}/available_payments`, (error, response, body) => {
+      if (error) return done(error);
+      const expectedResponse = {
+        payment_methods: {
+          credit_cards: true,
+          paypal: false
+        }
+      };
+      expect(JSON.parse(body)).to.deep.equal(expectedResponse);
+      done();
+    });
+  });
+
+  it('should return status code 200 for POST /login', function (done) {
+    request.post({
+      url: `${BASE_URL}/login`,
+      json: { userName: 'Betty' }
+    }, (error, response) => {
+      if (error) return done(error);
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('should return the correct message for POST /login', function (done) {
+    request.post({
+      url: `${BASE_URL}/login`,
+      json: { userName: 'Betty' }
+    }, (error, response, body) => {
+      if (error) return done(error);
+      expect(body).to.equal('Welcome Betty');
+      done();
+    });
+  });
+});
