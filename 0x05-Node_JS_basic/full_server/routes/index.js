f6ee02d920676b858bfd96e4402535a765fb7580
Pibo Shongwe <piboman@yahoo.com>
2024-07-27 09:08:07 +0000

    u

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
-import { AppController } from '../controllers/AppController';
-import { StudentsController } from '../controllers/StudentsController';
+import AppController from '../controllers/AppController';
+import StudentsController from '../controllers/StudentsController';
 
 const router = Router();
 
